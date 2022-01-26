24c848bf6fe714a20e2e541d5e2837d3d8a0be55
Yoni Kosiner <yoni@kosiner.co.uk>
2022-01-26 16:56:21 +0000

    Commit it gurllll

---
+++ main.py
@@ -0,0 +1,9 @@
+radius = float(input("Enter the radius of the circle: "))
+
+pi: float = 3.14
+
+area: float = pi * radius * radius
+perimeter: float = 2 * pi * radius
+
+print("The area of the circle is: ", area)
+print("yhe perimeter of the circle is: ", perimeter)
